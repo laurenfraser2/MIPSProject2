610eb72378f8e38581dd40dd8967775219a0654d
fLauren <laurenfraser222@gmail.com>
2018-11-16 13:09:06 -0500

    The next five lines create the label for checking the length of the input. Start the count at 0, save the current char into t1, and keep a0.

--- project2.s
+++ project2.s
@@ -9,11 +9,15 @@
 	la $a0, userInput
 	li $a1, 90000
 	syscall
-      leadingSpaces:  #Remove leading spaces
-	li $t8, 32      #Save space character to t8
-	lb $t9, 0($a0)
-	beq $t8, $t9, deleteSpace
-	move $t9, $a0
+	leadingSpaces:  #Remove leading spaces
+	 li $t8, 32      #Save space character to t8
+	 lb $t9, 0($a0)
+	 beq $t8, $t9, deleteSpace
+	 move $t9, $a0
 	deleteSpace: #initialize label to get rid of beginning spaces
 	 addi $a0, $a0, 1
 	 j leadingSpaces
+	checkLength:   #find the length of the input
+	 addi $t0, $t0, 0  #start count at zero
+	 addi $t1, $t1, 10  #put character into t1
+	 add $t4, $t4, $a0  #keep what was originally in a0
