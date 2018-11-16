bb2fa633dd28531c5d506778159618618d6b748c
fLauren <laurenfraser222@gmail.com>
2018-11-16 11:02:24 -0500

    leading spaces is a label that ensures the spaces in the beginning are not counted towards the four allowed chars.

--- project2.s
+++ project2.s
@@ -9,4 +9,9 @@
 	la $a0, userInput
 	li $a1, 90000
 	syscall
-      	
+      leadingSpaces:  #Remove leading spaces
+	li $t8, 32      #Save space character to t8
+	lb $t9, 0($a0)
+	beq $t8, $t9, deleteSpace
+	move $t9, $a0
+	j checkLength
