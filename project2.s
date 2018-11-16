ef04aee1a69567bb43884c87f191d696c82fb87b
fLauren <laurenfraser222@gmail.com>
2018-11-16 14:47:00 -0500

    set each char in the right position

--- project2.s
+++ project2.s
@@ -73,7 +73,11 @@
 	  move $a0, $t4
 	  addi $t7, $t7, 0  #Initialize decimal sum to zero
 	  add $s0, $s0, $t0
-	 
+	  addi $s0, $s0, -1	
+	  li $s3, 3 #position 3
+	  li $s2, 2
+	  li $s1, 1
+	  li $s5, 0
 	 
 	 
 
